5b363361567048a8f79f3e3af244e81b193d29ff
Vinicius Magrin <viniciusmagrin@gmail.com>
2026-07-19 10:51:15 -0300

    added new commands

--- comandos.sh
+++ comandos.sh
@@ -1,3 +1,8 @@
 pwd = print working directory
 ls = list files and folders
+ls -l = list the folders and files in long list format (more information)
 cd = change directory
+cd .. = up one folder * .. can be used in other situations too. More examples in the future
+more <filename> = see the content of the file
+clear = clear the terminal
+
