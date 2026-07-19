7ce6ed94c920b10cb1afbc0864dc6a5bb627e47d
Vinicius Magrin <viniciusmagrin@gmail.com>
2026-07-19 15:49:18 -0300

    final list of bash commands

--- comandos.sh
+++ comandos.sh
@@ -1,8 +1,50 @@
+echo message = prints message on the screen
+echo text >> <filename> = adds a text inside a file (in the last line, appending output)
+echo text > <filename> = overwrite the context of a file
 pwd = print working directory
 ls = list files and folders
 ls -l = list the folders and files in long list format (more information)
+ls -a or ls --all = list all files including hidden ones
 cd = change directory
-cd .. = up one folder * .. can be used in other situations too. More examples in the future
+cd .. = up one folder
+.. take you backup up one level
+cd ../.. go back up two folders
 more <filename> = see the content of the file
+less <filename> = view content one page at a time
+cat <filename> = show the entire content at once
 clear = clear the terminal
+mkdir <dirname> = create a new directory (folder) with <dirname> in the folder you are
+mkdir <foldername>/<newfoldername> = create a new folder on the path ex: mkdir client/src
+touch <filename> = create a new file named <filename>
+command --help = shows help about the command
+cp <file/folder> <destination> = copy the <file> to the <destination> ex: cp image.png images/image.png
+cp -r = to copy recursively
+rm <filename> = remove a file
+rm -r <foldername> = remove recursively the folder and its content (dangerous command)
+**** mv <filename> <destination> = to move a file. ex: mv image.jpg images/image.jpg
+mv <file/foldername> <newfilename> = to rename a file or folder
+mv <file/foldername> .. = move a file up one level
+find = show everything in the folder and in descedant folders (namefile and path)
+find <file/foldername> = display the tree of a different file folder or pattern (ex: "*.txt")
+find -name <file/foldername> = finds the file or folder by the exact name
+./ = represents this folder. ex: ./fonts/roboto.woff
+rmdir <dirname> = remove the directory
+tab = autocomplete text
+!! = instantly rerun the last executed command
+exit = exit the terminal
+man <command> = detailed manual / help ex: man ls or ls --help
+
+
+Command options or flags (long / short)
+-- help / -h
+--version / -v
+--width=50
+
+
+
+
+ 
+
+
+
 
